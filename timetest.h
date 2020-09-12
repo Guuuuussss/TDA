961065d96a89b2f47fbfe75191a4ca7e8b0e32c3
Gustavo <gustavo.luna.maya@gmail.com>
2020-09-12 18:10:31 -0500

    Entrega

---
+++ timetest.h
@@ -0,0 +1,20 @@
+//
+//  time_test_fibo.h
+//  
+//
+//  Created by Cesar Angeles on 07/09/2020.
+//
+
+#ifndef time_test_fibo_h
+#define time_test_fibo_h
+
+#include <stdio.h>
+#include "archivos.h"
+
+
+#define STATISTIC 
+#define OBSERVATIONS 25
+#define VALUES 3
+
+#endif
+
